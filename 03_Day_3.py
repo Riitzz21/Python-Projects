efa9aa156cb1ac94a666f8eb1508db59f856b2e2
Rajeshwari45 <rajeshwarivyas589@gmail.com>
2026-07-30 21:42:53 +0530

    Day 3 - Simple Bill Spliter

---
+++ 03_Day_3.py
@@ -0,0 +1,62 @@
+"""
+Challenge : Simple Bill Spliter
+
+Write a python script that helps split a bill evenly between friends.
+
+Your program should:
+1. Ask how many people are in the group.
+2. Ask for each person's name.
+3. Ask for the total bill amount.
+4. Calculate each person's share of the bill.
+5. Display how much each person owes in a clean, readable format.
+
+Example :
+Total bill: 1200 rupees
+People: Aman, Neha, Ravi, Ritzz
+
+Each person owes : 400 rupees
+
+Final Output : 
+
+Aman owes : 400 rupees
+Neha owes : 400 rupees
+Ravi owes : 400 rupees
+Ritzz owes : 400 rupees
+
+Bonus :
+- Round to 2 decimal places
+- Print a decorative summary box
+"""
+def get_float(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("❌ please enter a valid number.")
+
+num_people = int(input("How many people are there in the group ? "))
+names = []
+
+for i in range(num_people):
+    name = input(f"Enter the name of person one #{i+1}").strip()
+    names.append(name)
+
+total_bill = get_float("Enter the bill amount in number only.")
+
+share = round(total_bill / num_people, 2)
+
+print("\n" + "*" * 40)
+print(f"Total Bill: {total_bill}")
+print(f"Each person owes {share}")
+
+for name in names:
+    print(f"{name} owes {share} rupees.")
+
+print("\n" + "*" * 40)
+
+
+
+
+
+
+
